be044ecd446d7866c2cf0d88157df1dbec656dfd
lizafaria <farializa24@gmail.com>
2022-12-02 15:17:38 +0600

    added commit

---
+++ src/component/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <img
+        className="w-full h-full"
+        src="https://cdn.dribbble.com/users/1408464/screenshots/6377404/404_illustration_4x.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default ErrorPage;
