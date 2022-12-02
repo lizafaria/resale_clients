b05e8d871348499acdf785706eefc09f97c160ae
lizafaria <farializa24@gmail.com>
2022-12-02 15:10:32 +0600

    sixth commit

--- src/component/Advertise.js
+++ src/component/Advertise.js
@@ -1,11 +1,1 @@
-import React from "react";
 
-const Advertise = () => {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold">Advertisements</h1>
-    </div>
-  );
-};
-
-export default Advertise;
