2e162767094baa3cb04493f105525c44745ede47
lizafaria <farializa24@gmail.com>
2022-12-02 21:33:18 +0600

    Added

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -11,15 +11,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-  // apiKey: "AIzaSyBDzGhE6U_VMDCXwLxuLoPyhM4MRwZ5brQ",
-  // authDomain: "final-project-12085.firebaseapp.com",
-  // projectId: "final-project-12085",
-  // storageBucket: "final-project-12085.appspot.com",
-  // messagingSenderId: "52002703033",
-  // appId: "1:52002703033:web:3a2c5ac7d248ec02077d68",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app
+export default app;
